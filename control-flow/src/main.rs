b36bb7bc47c456210731e297f1d5ad60bce31499
sdornel <sdornel@numbersusa.com>
2025-04-08 16:42:33 -0400

    started on new func

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -4,6 +4,10 @@ fn main() {
     // Print the lyrics to the Christmas carol “The Twelve Days of Christmas,” taking advantage of the repetition in the song.
 }
 
+fn temperature_conversion (temp) {
+
+}
+
 // fn main() {
 //     let number = 7;
 
