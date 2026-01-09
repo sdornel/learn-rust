77687c85b9a1841b553f6e5188b0c0a0bbedb95f
sdornel <kai_dornel@hotmail.com>
2026-01-08 19:56:13 -0500

    updated

--- last-modified-time/src/main.rs
+++ last-modified-time/src/main.rs
@@ -5,17 +5,24 @@ fn main() {
     let path = "./tmp";
 
     let entries = fs::read_dir(path).expect("failed to read dir");
-    println!("entries: {:?}", entries);
+
+    let mut modified_since_threshold = false;
     for entry in entries {
         let entry = entry.expect("failed to get entry");
         let metadata = entry.metadata().expect("failed to get metadata");
         let modified_time = metadata.modified().expect("failed to get modified time");
-        println!("modified_time: {:?}", modified_time);
-        println!("5 mins ago: {:?}", SystemTime::now() - std::time::Duration::new(300, 0));
+
         if (modified_time) < (SystemTime::now() - std::time::Duration::new(300, 0)) {
-            println!("File: {} was modified more than 5 minutes ago", entry.path().display());
+            modified_since_threshold = true;
         } else {
-            println!("File: {} was modified within the last 5 minutes", entry.path().display());
+            modified_since_threshold = false;
+            break;
         }
+    }
+
+    if modified_since_threshold {
+        println!("No new files for more than 5 minutes!");
+    } else {
+        println!("Process continuing");
     }
 }
