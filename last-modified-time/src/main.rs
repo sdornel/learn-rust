ccb5fe3666de1cc928971dffc177f0ef1ef1e4d2
sdornel <kai_dornel@hotmail.com>
2026-01-09 20:47:32 -0500

    implement watcher

--- last-modified-time/src/main.rs
+++ last-modified-time/src/main.rs
@@ -4,10 +4,11 @@ use std::thread::sleep;
 
 fn main() {
     let path = "./tmp";
+    let mut alerted = false;
 
     loop {
         let entries = fs::read_dir(path).expect("failed to read dir");
-        let mut all_files_old = false;
+        let mut all_files_old = true; // assume all are old unless we find a new one
         let time = SystemTime::now();
         let cutoff = std::time::Duration::new(300, 0); // 5 minutes
         for entry in entries {
@@ -15,12 +16,10 @@ fn main() {
             let metadata = entry.metadata().expect("failed to get metadata");
             let modified_time = metadata.modified().expect("failed to get modified time");
     
-            if (modified_time) < (time - cutoff) {
-                all_files_old = true;
-            } else {
+            if (modified_time) > (time - cutoff) {
                 all_files_old = false;
                 break;
-            }
+            } 
         }
         
         if all_files_old {
