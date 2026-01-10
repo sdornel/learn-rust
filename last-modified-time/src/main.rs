37fc6fc15dfcd2c7dacb075428f1db288bad3ed3
sdornel <kai_dornel@hotmail.com>
2026-01-09 20:22:12 -0500

    monitoring loop

--- last-modified-time/src/main.rs
+++ last-modified-time/src/main.rs
@@ -1,30 +1,34 @@
 use std::fs;
-use std::time::SystemTime;
+use std::time::{SystemTime, Duration};
+use std::thread::sleep;
 
 fn main() {
     let path = "./tmp";
 
-    let entries = fs::read_dir(path).expect("failed to read dir");
-
-    let mut modified_since_threshold = false;
-    let time = SystemTime::now();
-    let cutoff = std::time::Duration::new(300, 0); // 5 minutes
-    for entry in entries {
-        let entry = entry.expect("failed to get entry");
-        let metadata = entry.metadata().expect("failed to get metadata");
-        let modified_time = metadata.modified().expect("failed to get modified time");
-
-        if (modified_time) < (time - cutoff) {
-            modified_since_threshold = true;
-        } else {
-            modified_since_threshold = false;
-            break;
+    loop {
+        let entries = fs::read_dir(path).expect("failed to read dir");
+        let mut all_files_old = false;
+        let time = SystemTime::now();
+        let cutoff = std::time::Duration::new(300, 0); // 5 minutes
+        for entry in entries {
+            let entry = entry.expect("failed to get entry");
+            let metadata = entry.metadata().expect("failed to get metadata");
+            let modified_time = metadata.modified().expect("failed to get modified time");
+    
+            if (modified_time) < (time - cutoff) {
+                all_files_old = true;
+            } else {
+                all_files_old = false;
+                break;
+            }
         }
+        
+        if all_files_old {
+            println!("No new files for more than 5 minutes!");
+        } else {
+            println!("Process continuing");
+        }
+        sleep(Duration::from_secs(60));
     }
 
-    if modified_since_threshold {
-        println!("No new files for more than 5 minutes!");
-    } else {
-        println!("Process continuing");
-    }
 }
