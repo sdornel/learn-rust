5194510affa3c6accd212a35680f6b81d3ad04d3
sdornel <kai_dornel@hotmail.com>
2026-01-09 20:00:06 -0500

    cleanup

--- last-modified-time/src/main.rs
+++ last-modified-time/src/main.rs
@@ -7,12 +7,14 @@ fn main() {
     let entries = fs::read_dir(path).expect("failed to read dir");
 
     let mut modified_since_threshold = false;
+    let time = SystemTime::now();
+    let cutoff = std::time::Duration::new(300, 0); // 5 minutes
     for entry in entries {
         let entry = entry.expect("failed to get entry");
         let metadata = entry.metadata().expect("failed to get metadata");
         let modified_time = metadata.modified().expect("failed to get modified time");
 
-        if (modified_time) < (SystemTime::now() - std::time::Duration::new(300, 0)) {
+        if (modified_time) < (time - cutoff) {
             modified_since_threshold = true;
         } else {
             modified_since_threshold = false;
