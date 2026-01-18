ec841f643b12602a6fa9921c7f389c4ad52463b9
sdornel <kai_dornel@hotmail.com>
2026-01-18 18:47:30 -0500

    show "current" weather

--- weather-cli/src/main.rs
+++ weather-cli/src/main.rs
@@ -1,11 +1,29 @@
 mod models;
-use models::Location;
+use models::{
+    Location,
+    CoordinatesResponse,
+    Coordinates,
+    WeatherResponse,
+    WeatherData,
+};
+use urlencoding::encode;
 
 fn main() {
-    location();
+    let city: String = location();
+    let coordinates: Coordinates = location_cordinates(&city);
+    println!("Fetching weather for {}, lat: {:?}, lon: {:?}", city, coordinates.latitude, coordinates.longitude);
+    let current_weather = current_weather(coordinates.latitude, coordinates.longitude);
+    println!("");
+    // for some reason the API doesn't always get current time?
+    println!("Fetching weather for {} at time {} (local time)", city, current_weather.time);
+    println!("Current weather in {}: {} {}",
+        city,
+        current_weather.temperature,
+        current_weather.unit
+    );
 }
 
-fn location() {
+fn location() -> String {
     let response = reqwest::blocking::get("https://ipinfo.io/json").unwrap();
 
     let location: Location = response.json().unwrap();
@@ -18,4 +36,34 @@ fn location() {
     println!("Postal Code: {}", location.postal);
     println!("Timezone: {}", location.timezone);
     println!("Readme: {}", location.readme);
+    println!("---------------------------");
+    location.city
+}
+
+fn location_cordinates(city: &str) -> Coordinates {
+    let url = format!("https://geocoding-api.open-meteo.com/v1/search?name={}&count=1&language=en&format=json", encode(city));
+    let response: CoordinatesResponse = reqwest::blocking::get(&url)
+        .unwrap()
+        .json()
+        .unwrap();
+
+    Coordinates {
+        latitude: response.results[0].latitude,
+        longitude: response.results[0].longitude,
+    }
+}
+
+fn current_weather(latitude: f64, longitude: f64) -> WeatherData {
+    let url = format!("https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&current_weather=true", latitude, longitude);
+    let response: WeatherResponse = reqwest::blocking::get(&url)
+        .unwrap()
+        .json()
+        .unwrap();
+
+    WeatherData {
+        time: response.current_weather.time,
+        unit: response.current_weather_units.temperature,
+        temperature: response.current_weather.temperature,
+        timezone: response.timezone,
+    }
 }
--- weather-cli/src/models.rs
+++ weather-cli/src/models.rs
@@ -12,3 +12,46 @@ pub struct Location {
     pub timezone: String,
     pub readme: String,
 }
+
+#[derive(Deserialize, Debug)]
+pub struct CoordinatesResponse {
+    pub results: Vec<Coordinates>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Coordinates {
+    pub latitude: f64,
+    pub longitude: f64,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct WeatherResponse {
+    pub timezone: String,
+    pub current_weather_units: CurrentWeatherUnits,
+    pub current_weather: CurrentWeather,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct CurrentWeatherUnits {
+    pub temperature: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct CurrentWeather {
+    pub time: String,
+    pub temperature: f64,
+    pub windspeed: f64,
+    pub winddirection: f64,
+    pub is_day: u8,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct WeatherData {
+    pub time: String,
+    pub temperature: f64,
+    pub unit: String,
+    pub timezone: String,
+    // pub windspeed: f64,
+    // pub winddirection: f64,
+    // pub is_day: u8,
+}
